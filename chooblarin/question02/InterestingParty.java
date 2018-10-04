5aaa405257cc3b5ed4d144d066e66cf0cb9ca9e0
chooblarin <choo.bla.rin@gmail.com>
2018-10-04 10:33:15 +0900

    Hello, I'm Hatakeyama

---
+++ chooblarin/question02/InterestingParty.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Collections;
+
+public class InterestingParty {
+    public int bestInvitation(String[] first, String[] second) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = 0; i < first.length; i++) {
+            String fst = first[i];
+            int n = (map.get(fst) == null) ? 0 : map.get(fst);
+            map.put(fst, n + 1);
+
+            String snd = second[i];
+            n = (map.get(snd) == null) ? 0 : map.get(snd);
+            map.put(snd, n + 1);
+        }
+        return Collections.max(map.values());
+    }
+}
