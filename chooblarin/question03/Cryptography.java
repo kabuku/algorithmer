701c153b96cc2fc786d8009abfa5481b4107bc2c
Sota <choo.bla.rin@gmail.com>
2018-10-05 10:06:15 +0900

    Reformat Java code

--- chooblarin/question03/Cryptography.java
+++ chooblarin/question03/Cryptography.java
@@ -1,17 +1,17 @@
 public class Cryptography {
-    public long encrypt(int[] numbers) {
-        long m = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            long p = 1;
-            for (int j = 0; j < numbers.length; j++) {
-                if (i == j) {
-                    p *= numbers[j] + 1;
-                } else {
-                    p *= numbers[j];
-                }
-            }
-            m = Math.max(m, p);
+  public long encrypt(int[] numbers) {
+    long m = 0;
+    for (int i = 0; i < numbers.length; i++) {
+      long p = 1;
+      for (int j = 0; j < numbers.length; j++) {
+        if (i == j) {
+          p *= numbers[j] + 1;
+        } else {
+          p *= numbers[j];
         }
-        return m;
+      }
+      m = Math.max(m, p);
     }
+    return m;
+  }
 }
